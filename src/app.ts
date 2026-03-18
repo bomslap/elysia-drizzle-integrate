752ecbfe53bdecc27b72b020605ed5b8e65d71c9
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2026-03-18 18:18:34 +0700

    add template rendering for email

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import Elysia from "elysia";
 import openapi from "@elysiajs/openapi";
 import itemsApi from "./features/items/items.api";
+import ejs from "ejs";
 
-const api = new Elysia({ prefix: "/api" }).use(itemsApi);
+const api = new Elysia({ prefix: "/api" }).use(itemsApi).get("/:name", ({ params }) => ejs.renderFile("./src/features/mails/templates/template1.ejs", params));
 
 const app = new Elysia().use(openapi()).use(api);
 
